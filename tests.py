4b771f735aee7d7d35538cf9aef3ab89da8ec340
Andrey Antukh <niwi@niwi.be>
2013-05-20 21:01:56 +0200

    Improvements on tests.

--- tests.py
+++ tests.py
@@ -2,18 +2,24 @@
 
 import unittest
 import time
+from os.path import join, dirname, abspath
 
 import phantompy as ph
+
+CURRENT_DIR = dirname(abspath(__file__))
+
+TEST_FILE = join(CURRENT_DIR, "misc", "test.html")
+TEST_FILE_WITH_FRAMES = join(CURRENT_DIR, "misc", "test_with_frames.html")
 
 
 class WebPageTests(unittest.TestCase):
     def test_load_page(self):
-        frame = ph.open("http://127.0.0.1/test.html")
+        frame = ph.open(TEST_FILE)
         html_data = frame.to_html()
         self.assertNotEqual(len(html_data), 0)
 
     def test_evaluate_js(self):
-        frame = ph.open("http://127.0.0.1/test.html")
+        frame = ph.open(TEST_FILE)
         result = frame.evaluate('document.getElementsByTagName("h1")[0].innerHTML')
         self.assertEqual(result, "Title")
 
@@ -23,7 +29,7 @@ class WebPageTests(unittest.TestCase):
         self.assertIsInstance(cookies, dict)
 
     def test_page_all_requests(self):
-        frame = ph.open("http://127.0.0.1/test.html")
+        frame = ph.open(TEST_FILE)
         self.assertEqual(frame.page.requested_urls(),
             ['http://code.jquery.com/jquery-1.9.1.min.js'])
         self.assertEqual(len(frame.page.requested_urls()), 1)
@@ -31,27 +37,27 @@ class WebPageTests(unittest.TestCase):
 
 class WebElementTests(unittest.TestCase):
     def test_get_title_element(self):
-        p = ph.open("http://127.0.0.1/test.html")
+        p = ph.open(TEST_FILE)
 
         element = p.cssselect("title")
         self.assertEqual(element.name, "TITLE")
         self.assertEqual(element.inner_html(), "simple title")
 
     def test_get_null_element(self):
-        p = ph.open("http://127.0.0.1/test.html")
+        p = ph.open(TEST_FILE)
 
         element = p.cssselect("foo")
         self.assertTrue(element.is_none())
 
     def test_get_inner_html(self):
-        p = ph.open("http://127.0.0.1/test.html")
+        p = ph.open(TEST_FILE)
 
         element = p.cssselect("#footer")
         html = element.inner_html()
         self.assertEqual(html.strip(), "<span>some footer</span>")
 
     def test_get_inner_text(self):
-        p = ph.open("http://127.0.0.1/test.html")
+        p = ph.open(TEST_FILE)
 
         element = p.cssselect("#footer")
         text = element.inner_text()
@@ -60,7 +66,7 @@ class WebElementTests(unittest.TestCase):
 
 class WebPageImageTests(unittest.TestCase):
     def test_capture_page(self):
-        p = ph.open("http://127.0.0.1/test.html")
+        p = ph.open(TEST_FILE)
         img = p.to_image()
         blob = img.to_blob()
 
