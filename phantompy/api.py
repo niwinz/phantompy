dbd09a7c13457d63b07adc06116594b6f7a17d02
Andrey Antukh <niwi@niwi.be>
2013-05-25 14:42:49 +0200

    Add new c library dom api to ctypes definition.

--- phantompy/api.py
+++ phantompy/api.py
@@ -2,6 +2,7 @@
 
 import ctypes
 import ctypes.util
+from ctypes import *
 import os
 import json
 
@@ -14,122 +15,158 @@ try:
     _library = load_library()
     # Context methods
     _library.ph_context_init.argtypes = []
-    _library.ph_context_init.restype = ctypes.c_void_p
+    _library.ph_context_init.restype = c_void_p
 
-    _library.ph_context_free.argtypes = [ctypes.c_void_p]
+    _library.ph_context_free.argtypes = [c_void_p]
     _library.ph_context_free.restype = None
 
     _library.ph_context_clear_memory_cache.argtypes = []
     _library.ph_context_clear_memory_cache.restype = None
 
-    _library.ph_context_set_object_cache_capacity.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
+    _library.ph_context_set_object_cache_capacity.argtypes = [c_int, c_int, c_int]
     _library.ph_context_set_object_cache_capacity.restype = None
 
-    _library.ph_context_set_max_pages_in_cache.argtypes = [ctypes.c_int]
+    _library.ph_context_set_max_pages_in_cache.argtypes = [c_int]
     _library.ph_context_set_max_pages_in_cache.restype = None
 
     # Page methods
     _library.ph_page_create.argtypes = []
-    _library.ph_page_create.restype = ctypes.c_void_p
+    _library.ph_page_create.restype = c_void_p
 
-    _library.ph_page_set_viewpoint_size.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
+    _library.ph_page_set_viewpoint_size.argtypes = [c_void_p, c_int, c_int]
     _library.ph_page_set_viewpoint_size.restype = None
 
-    _library.ph_page_free.argtypes = [ctypes.c_void_p]
+    _library.ph_page_free.argtypes = [c_void_p]
     _library.ph_page_free.restype = None
 
-    _library.ph_page_load.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_page_load.restype = ctypes.c_int
+    _library.ph_page_load.argtypes = [c_void_p, c_char_p]
+    _library.ph_page_load.restype = c_int
 
-    _library.ph_page_is_loaded.argtypes = [ctypes.c_void_p]
-    _library.ph_page_is_loaded.restype = ctypes.c_int
+    _library.ph_page_is_loaded.argtypes = [c_void_p]
+    _library.ph_page_is_loaded.restype = c_int
 
-    _library.ph_page_cookies.argtypes = [ctypes.c_void_p]
-    _library.ph_page_cookies.restype = ctypes.c_void_p
+    _library.ph_page_cookies.argtypes = [c_void_p]
+    _library.ph_page_cookies.restype = c_void_p
     _library.ph_page_cookies._char_ptr = True
 
-    _library.ph_page_requested_urls.argtypes = [ctypes.c_void_p]
-    _library.ph_page_requested_urls.restype = ctypes.c_void_p
+    _library.ph_page_requested_urls.argtypes = [c_void_p]
+    _library.ph_page_requested_urls.restype = c_void_p
     _library.ph_page_requested_urls._char_ptr = True
 
     # Frame methods
-    _library.ph_page_main_frame.argtypes = [ctypes.c_void_p]
-    _library.ph_page_main_frame.restype = ctypes.c_void_p
+    _library.ph_page_main_frame.argtypes = [c_void_p]
+    _library.ph_page_main_frame.restype = c_void_p
 
-    _library.ph_frame_free.argtypes = [ctypes.c_void_p]
+    _library.ph_frame_free.argtypes = [c_void_p]
     _library.ph_frame_free.restype = None
 
-    _library.ph_frame_to_html.argtypes = [ctypes.c_void_p]
-    _library.ph_frame_to_html.restype = ctypes.c_void_p
+    _library.ph_frame_to_html.argtypes = [c_void_p]
+    _library.ph_frame_to_html.restype = c_void_p
     _library.ph_frame_to_html._char_ptr = True
 
-    _library.ph_frame_evaluate_javascript.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_frame_evaluate_javascript.restype = ctypes.c_void_p
+    _library.ph_frame_evaluate_javascript.argtypes = [c_void_p, c_char_p]
+    _library.ph_frame_evaluate_javascript.restype = c_void_p
     _library.ph_frame_evaluate_javascript._char_ptr = True
 
     # Image methods
-    _library.ph_frame_capture_image.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_int]
-    _library.ph_frame_capture_image.restype = ctypes.c_void_p
+    _library.ph_frame_capture_image.argtypes = [c_void_p, c_char_p, c_int]
+    _library.ph_frame_capture_image.restype = c_void_p
 
-    _library.ph_image_get_size.argtypes = [ctypes.c_void_p]
-    _library.ph_image_get_size.restype = ctypes.c_longlong
+    _library.ph_image_get_size.argtypes = [c_void_p]
+    _library.ph_image_get_size.restype = c_longlong
 
-    _library.ph_image_get_format.argtypes = [ctypes.c_void_p]
-    _library.ph_image_get_format.restype = ctypes.c_char_p
+    _library.ph_image_get_format.argtypes = [c_void_p]
+    _library.ph_image_get_format.restype = c_char_p
 
-    _library.ph_image_free.argtypes = [ctypes.c_void_p]
+    _library.ph_image_free.argtypes = [c_void_p]
     _library.ph_image_free.restype = None
 
-    _library.ph_image_get_bytes.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_longlong]
+    _library.ph_image_get_bytes.argtypes = [c_void_p, c_void_p, c_longlong]
     _library.ph_image_get_bytes.restype = None
 
     # Web Element Collection methods
+    _library.ph_webcollection_size.argtypes = [c_void_p]
+    _library.ph_webcollection_size.restype = c_int
 
-    _library.ph_webcollection_size.argtypes = [ctypes.c_void_p]
-    _library.ph_webcollection_size.restype = ctypes.c_int
+    _library.ph_webcollection_get_webelement.argtypes = [c_void_p, c_int]
+    _library.ph_webcollection_get_webelement.restype = c_void_p
 
-    _library.ph_webcollection_get_webelement.argtypes = [ctypes.c_void_p, ctypes.c_int]
-    _library.ph_webcollection_get_webelement.restype = ctypes.c_void_p
-
-    _library.ph_webcollection_free.argtypes = [ctypes.c_void_p]
+    _library.ph_webcollection_free.argtypes = [c_void_p]
     _library.ph_webcollection_free.restype = None
 
     # Web Element methods
-    _library.ph_frame_find_first.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_frame_find_first.restype = ctypes.c_void_p
+    _library.ph_frame_find_all.argtypes = [c_void_p, c_char_p]
+    _library.ph_frame_find_all.restype = c_void_p
 
-    _library.ph_frame_find_all.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_frame_find_all.restype = ctypes.c_void_p
-
-    _library.ph_webelement_free.argtypes = [ctypes.c_void_p]
+    _library.ph_webelement_free.argtypes = [c_void_p]
     _library.ph_webelement_free.restype = None
 
-    _library.ph_webelement_tag_name.argtypes = [ctypes.c_void_p]
-    _library.ph_webelement_tag_name.restype = ctypes.c_void_p
+    _library.ph_webelement_tag_name.argtypes = [c_void_p]
+    _library.ph_webelement_tag_name.restype = c_void_p
     _library.ph_webelement_tag_name._char_ptr = True
 
-    _library.ph_webelement_inner_html.argtypes = [ctypes.c_void_p]
-    _library.ph_webelement_inner_html.restype = ctypes.c_void_p
+    _library.ph_webelement_inner_html.argtypes = [c_void_p]
+    _library.ph_webelement_inner_html.restype = c_void_p
     _library.ph_webelement_inner_html._char_ptr = True
 
-    _library.ph_webelement_inner_text.argtypes = [ctypes.c_void_p]
-    _library.ph_webelement_inner_text.restype = ctypes.c_void_p
+    _library.ph_webelement_inner_text.argtypes = [c_void_p]
+    _library.ph_webelement_inner_text.restype = c_void_p
     _library.ph_webelement_inner_text._char_ptr = True
 
-    _library.ph_webelement_find_all.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_webelement_find_all.restype = ctypes.c_void_p
+    _library.ph_webelement_find_all.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_find_all.restype = c_void_p
 
-    _library.ph_webelement_has_class.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_webelement_has_class.restype = ctypes.c_int
+    _library.ph_webelement_has_class.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_has_class.restype = c_int
 
-    _library.ph_webelement_has_attr.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-    _library.ph_webelement_has_attr.restype = ctypes.c_int
+    _library.ph_webelement_add_class.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_add_class.restype = None
 
-    _library.ph_webelement_has_attrs.argtypes = [ctypes.c_void_p]
-    _library.ph_webelement_has_attrs.restype = ctypes.c_int
+    _library.ph_webelement_remove_class.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_remove_class.restype = None
 
-    _library.ph_webelement_is_null.argtypes = [ctypes.c_void_p]
-    _library.ph_webelement_is_null.restype = ctypes.c_int
+    _library.ph_webelement_set_attr.argtypes = [c_void_p, c_char_p, c_char_p]
+    _library.ph_webelement_set_attr.restype = None
+
+    _library.ph_webelement_has_attr.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_has_attr.restype = c_int
+
+    _library.ph_webelement_remove_attr.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_remove_attr.restype = None
+
+    _library.ph_webelement_has_attrs.argtypes = [c_void_p]
+    _library.ph_webelement_has_attrs.restype = c_int
+
+    _library.ph_webelement_is_null.argtypes = [c_void_p]
+    _library.ph_webelement_is_null.restype = c_int
+
+    _library.ph_webelement_append_html.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_append_html.restype = None
+    _library.ph_webelement_append_element.argtypes = [c_void_p, c_void_p]
+    _library.ph_webelement_append_element.restype = None
+    _library.ph_webelement_append_html_after.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_append_html_after.restype = None
+    _library.ph_webelement_append_element_after.argtypes = [c_void_p, c_void_p]
+    _library.ph_webelement_append_element_after.restype = None
+    _library.ph_webelement_replace_with_html.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_replace_with_html.restype = None
+    _library.ph_webelement_replace_with_element.argtypes = [c_void_p, c_void_p]
+    _library.ph_webelement_replace_with_element.restype = None
+    _library.ph_webelement_remove_all_child_elements.argtypes = [c_void_p]
+    _library.ph_webelement_remove_all_child_elements.restype = None
+    _library.ph_webelement_remove_from_document.argtypes = [c_void_p]
+    _library.ph_webelement_remove_from_document.restype = None
+    _library.ph_webelement_take_from_document.argtypes = [c_void_p]
+    _library.ph_webelement_take_from_document.restype = c_void_p
+    _library.ph_webelement_wrap_with_html.argtypes = [c_void_p, c_char_p]
+    _library.ph_webelement_wrap_with_html.restype = None
+    _library.ph_webelement_wrap_with_element.argtypes = [c_void_p, c_void_p]
+    _library.ph_webelement_wrap_with_element.restype = None
+    _library.ph_webelement_previous.argtypes = [c_void_p]
+    _library.ph_webelement_previous.restype = c_void_p
+    _library.ph_webelement_next.argtypes = [c_void_p]
+    _library.ph_webelement_next.restype = c_void_p
+
 
 except AttributeError:
     raise ImportError('shared library not found or incompatible')
