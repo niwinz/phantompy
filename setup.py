106c8c407d22fae54d41e9bd1c009f63116f5abe
Andrey Antukh <niwi@niwi.be>
2013-06-01 19:38:24 +0200

    Increment version to 0.4

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ class build(distutils.command.build.build):
 
 setup(
     name = 'phantompy',
-    version = "0.3",
+    version = "0.4",
     description = "Headless WebKit engine for python build on top of Qt5 and Webkit",
     long_description = "",
     keywords = 'webkit, headless, qt, engine',
